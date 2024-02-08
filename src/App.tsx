fc39e598254e0c7d2bbc541f6e38d00f4e501669
Silvia9680 <silviacarolina9680@gmail.com>
2024-02-08 09:17:19 -0300

    Navbar finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
-
-
+import { useState } from "react";
+import Navbar from "./scenes/navbar";
 
 function App() {
 
+  const [selectedPage, setSelectedPage] = useState("home");
 
-  return (
-    <div className='app' >
-        app
+  return(
+    <div className="app bg-gray-20">
+        <Navbar 
+          selectedPage={selectedPage}
+          setSelectedPage={setSelectedPage}
+        />
     </div>
   )
 }
 
-export default App
+export default App;
